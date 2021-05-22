54f6029412901bc89e31ace2cdb795e8225088cd
Rodolfo Talan <rtalan@gmail.com>
2021-05-22 13:24:55 -0600

    NavBar Interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+
+export class AppComponent implements OnInit {
+  title = 'RTArts';
+  products: IProduct[];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
